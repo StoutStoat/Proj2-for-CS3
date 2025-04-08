5bd67fcbb8e690d1635acc69796e562c49324a89
StoutStoat <ethanjoung11@gmail.com>
2025-04-08 12:17:23 -0500

    UNFINISHED

--- cards.java
+++ cards.java
@@ -1,24 +1,34 @@
 /**
  * Created: 4/8/2025 (DD/MM/YYYY)
  * 
- * This class handles all the 52 cards. 
+ * This class handles all 52 cards. 
  */
+import java.util.LinkedList;
 
 public class cards {
+
+    LinkedList<String> cardDeck;
 
     public cards()
     {
     //initialize deckMap
-
+        cardDeck = new LinkedList<String>();
     }
 
     public void createDeck()
     {
-        //
-        
+        //4 suites, 13 numbers. Jack, Queen & King
+        for (int i = 1; i < 5; i++)
+        {
+            for (int j = 1; j < 14; j++)
+            {
+                cardDeck.add(i);
+            }
+        } 
         //then assign value
         assignValue();
     }
+
     /**
      * Assigns a value from 1 to 13 depending on the card.
      * Ace (1) to King (13)
