f178efa0e1ee291140cf6b7f9cddf0cc8043a7f7
StoutStoat <ethanjoung11@gmail.com>
2025-04-08 11:50:46 -0500

    Created Cards class

---
+++ cards.java
@@ -0,0 +1,41 @@
+/**
+ * Created: 4/8/2025 (DD/MM/YYYY)
+ * 
+ * This class handles all the 52 cards. 
+ */
+
+public class cards {
+
+    public cards()
+    {
+    //initialize deckMap
+
+    }
+
+    public void createDeck()
+    {
+        //
+        
+        //then assign value
+        assignValue();
+    }
+    /**
+     * Assigns a value from 1 to 13 depending on the card.
+     * Ace (1) to King (13)
+     */
+    private void assignValue()
+    {
+
+    };
+    
+    /**
+     * returns value of card.
+     * @return
+     */
+    public int returnValue()
+    {
+        return 0;
+    };
+
+}
+
