cef4150fe3d7592351013885e6ee9a6ea262fbba
StoutStoat <ethanjoung11@gmail.com>
2025-04-14 10:51:59 -0500

    bleh

--- cardDeck.java
+++ cardDeck.java
@@ -7,6 +7,7 @@
 import java.util.*;
 
 public class cardDeck {
+    //replace with set list
     ArrayList<card> cards = new ArrayList<card>();
 
     String[] values = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
