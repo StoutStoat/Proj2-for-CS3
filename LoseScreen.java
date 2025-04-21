c902dcb0ffae025e158daa76a5e870342fc6bc30
InactiveGaming <149812946+InactiveGaming@users.noreply.github.com>
2025-04-21 12:14:22 -0500

    Update LoseScreen.java
    added in a delay to show the game over screen and then the options

--- LoseScreen.java
+++ LoseScreen.java
@@ -1,9 +1,17 @@
 import mayflower.*;
+import java.util.Date;
 
 public class LoseScreen extends World{
 	MouseInfo minfo = Mayflower.getMouseInfo();
     public LoseScreen() {
         // you lose!!!!
+	setBackground("images/ExampleGameOver.png");
+	try{
+		Thread.sleep(4000);
+	} catch (InterruptedException e){
+		e.printStackTrace();
+	}
+	setBackground("images/White.jpeg");
         showText("You Lose!", 300, 150, Color.BLACK);
         showText("Retry? [1]", 300, 200, Color.BLACK);
         showText("Main Menu [2]", 300, 250, Color.BLACK);
